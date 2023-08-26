9cf8add65dec6159d929576a030f12164389555b
Stephens37 <braydenstephens@protonmail.com>
2023-08-26 08:43:25 -0500

    'About to work on polyfill error'

--- restaurant.git/webpack.config.js
+++ restaurant.git/webpack.config.js
@@ -2,10 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
   
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'restaurant.git./dist'),
-    filename: 'main_bundle.js',
+
+  devtool: 'inline-source-map',
+  devServer: {
+
+    static: './dist',
+
   },
   plugins: [
 
@@ -13,7 +17,33 @@ module.exports = {
 
       title: 'Restaurant',
 
-    }),
+    })
 
   ],
-};
+
+  module: {
+    rules: [
+      // Define rules for processing different types of files
+      {
+        test: /\.js$/, // Apply the rule to .js files
+        exclude: /node_modules/, // Exclude files from node_modules directory
+        use: {
+          loader: 'babel-loader', // Use Babel loader for .js files
+        },
+      },
+      // Add more rules for other file types like stylesheets, images, etc.
+    ],
+  },
+  // Other configurat
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+
+    runtimeChunk: 'single',
+
+  },
+}
