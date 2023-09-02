d6fb07820fb150f1a3bf3ac2b00d53e6638080fc
Stephens37 <braydenstephens@protonmail.com>
2023-09-02 10:39:33 -0500

    'Work on editing homepage'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import { component } from './elements.js'
 
 component()
+
+function header () {
+  const header = document.createElement('header')
+
+  const home = document.createElement('button')
+  home.classList('buttons')
+  home.setAttribute('id', 'homebutton')
+  home.addEventListener(component)
+
+  const menu = document.createElement('button')
+  menu.classList('buttons')
+  menu.setAttribute('id', 'menubutton')
+
+  const contact = document.createElement('button')
+  contact.classList('contact')
+  contact.setAttribute('id', 'contactbutton')
+
+  header.appendChild(home)
+  header.appendChild(menu)
+  header.appendChild(contact)
+}
+header()
+
+function footer () {
+  const footer = document.createElement('footer')
+
+  const resource1 = document.createElement('div')
+  resource1.innerText = 'Resource'
+  footer.append(resource1)
+}
+footer()
