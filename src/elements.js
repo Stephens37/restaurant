c73e5fdd98e26715052ff4295cfad3d530662616
Stephens37 <braydenstephens@protonmail.com>
2023-09-02 11:09:34 -0500

    'Work on append error'

--- src/elements.js
+++ src/elements.js
@@ -2,15 +2,7 @@ import Pasta from './pasta.jpeg'
 import _ from 'lodash'
 require('./style.css')
 
-const body = document.getElementsByTagName('body')
-let contentParent = document.createElement('div')
-contentParent = document.querySelector('id', '#contentparent')
 const content = document.querySelector('#content')
-
-export function frame () {
-  body.appendChild(contentParent)
-  contentParent.appendChild(content)
-}
 
 export function component () {
   const header = document.createElement('header');
--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
+import _ from 'lodash';
 import { component } from './elements.js'
 
 component()
 
+const body = document.getElementsByTagName('body')
+let contentParent = document.createElement('div')
+contentParent = document.querySelector('id', '#contentparent')
+const content = document.querySelector('#content')
+
+function frame () {
+  body.append(contentParent)
+  contentParent.append(content)
+}
+//remember up here
 function header () {
   const header = document.createElement('header')
 
   const home = document.createElement('button')
-  home.classList('buttons')
+  home.setAttribute('class', '.buttons')
   home.setAttribute('id', 'homebutton')
-  home.addEventListener(component)
+  home.addEventListener('click', component)
 
   const menu = document.createElement('button')
-  menu.classList('buttons')
+  menu.setAttribute('class', '.buttons')
   menu.setAttribute('id', 'menubutton')
 
   const contact = document.createElement('button')
-  contact.classList('contact')
+  contact.setAttribute('class', 'contact')
   contact.setAttribute('id', 'contactbutton')
 
   header.appendChild(home)
